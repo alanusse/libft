fe629c9ff195ddb634628da8cf42bb2acf7365ee
Agustin Lanusse <lanusseagustin@gmail.com>
2023-09-13 11:14:31 +0200

    ft_isdigit

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,19 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/13 10:44:20 by aglanuss          #+#    #+#             */
-/*   Updated: 2023/09/13 11:07:00 by aglanuss         ###   ########.fr       */
+/*   Created: 2023/09/13 11:07:20 by aglanuss          #+#    #+#             */
+/*   Updated: 2023/09/13 11:13:18 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-
-#endif
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
