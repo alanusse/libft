82f7c9c092253c9e375a5eec6fce0d7d7d4cfb1c
Agustin Lanusse <lanusseagustin@gmail.com>
2023-09-14 18:46:03 +0200

    added ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,27 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/13 10:44:20 by aglanuss          #+#    #+#             */
-/*   Updated: 2023/09/14 18:45:40 by aglanuss         ###   ########.fr       */
+/*   Created: 2023/09/14 18:11:16 by aglanuss          #+#    #+#             */
+/*   Updated: 2023/09/14 18:45:07 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdlib.h>
+#include <stdlib.h>
 
-int			ft_isalpha(int c);
-int			ft_isdigit(int c);
-int			ft_isalnum(int c);
-int			ft_isascii(int c);
-int			ft_isprint(int c);
-size_t		ft_strlen(const char *s);
-void		*ft_memset(void *b, int c, size_t len);
-void		*ft_memcpy(void *dst, const void *src, size_t n);
-void		ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+	char	*source;
+	char	*destination;
 
-#endif
+	i = 0;
+	source = (char *)src;
+	destination = (char *)dst;
+	while (i < n)
+	{
+		destination[i] = source[i];
+		i++;
+	}
+	return (dst);
+}
