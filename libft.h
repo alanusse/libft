3d43bf7c63a7f1ca3f63cb166944c506f23a0abb
Agustin Lanusse <lanusseagustin@gmail.com>
2023-09-14 19:12:13 +0200

    ft_tolower

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 10:44:20 by aglanuss          #+#    #+#             */
-/*   Updated: 2023/09/14 18:48:01 by aglanuss         ###   ########.fr       */
+/*   Updated: 2023/09/14 19:00:13 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int			ft_isalnum(int c);
 int			ft_isascii(int c);
 int			ft_isprint(int c);
 int			ft_toupper(int c);
+int			ft_tolower(int c);
 size_t		ft_strlen(const char *s);
 void		*ft_memset(void *b, int c, size_t len);
 void		*ft_memcpy(void *dst, const void *src, size_t n);
