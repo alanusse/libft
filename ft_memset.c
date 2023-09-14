7885124f27400daeab6700c1eed613180fcd5aa1
Agustin Lanusse <lanusseagustin@gmail.com>
2023-09-14 15:46:17 +0200

    ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,38 +6,22 @@
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 13:38:49 by aglanuss          #+#    #+#             */
-/*   Updated: 2023/09/13 14:32:00 by aglanuss         ###   ########.fr       */
+/*   Updated: 2023/09/14 15:45:48 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
-void *ft_memset(void *b, int c, size_t len)
+void	*ft_memset(void *b, int c, size_t len)
 {
-  size_t bytes_writed;
-  char *pos;
+	char	*pos;
 
-  bytes_writed = 0;
-  pos = (char *) b;
-  while(bytes_writed < len)
-  {
-    *pos = c;
-    b++;
-    bytes_writed += sizeof(c);
-  }
-  return b;
-}
-
-int main()
-{
-    char str[50] = "GeeksForGeeks is for programming geeks.";
-    printf("\nBefore memset(): %s\n", str);
-  
-    // Fill 8 characters starting from str[13] with '.'
-    ft_memset(str + 13, '.', 8*sizeof(char));
-  
-    printf("After memset():  %s", str);
-    return 0;
+	pos = b;
+	while (len > 0)
+	{
+		*pos = c;
+		len--;
+		pos++;
+	}
+	return (b);
 }
