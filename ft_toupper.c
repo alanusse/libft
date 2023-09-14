b47875316351094209c78de1f9b45f9966006568
Agustin Lanusse <lanusseagustin@gmail.com>
2023-09-14 18:59:25 +0200

    ft_toupper

--- ft_toupper.c
+++ ft_toupper.c
@@ -1,28 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/13 10:44:20 by aglanuss          #+#    #+#             */
-/*   Updated: 2023/09/14 18:48:01 by aglanuss         ###   ########.fr       */
+/*   Created: 2023/09/14 18:46:34 by aglanuss          #+#    #+#             */
+/*   Updated: 2023/09/14 18:57:10 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdlib.h>
+#include <stdio.h>
 
-int			ft_isalpha(int c);
-int			ft_isdigit(int c);
-int			ft_isalnum(int c);
-int			ft_isascii(int c);
-int			ft_isprint(int c);
-int			ft_toupper(int c);
-size_t		ft_strlen(const char *s);
-void		*ft_memset(void *b, int c, size_t len);
-void		*ft_memcpy(void *dst, const void *src, size_t n);
-void		ft_bzero(void *s, size_t n);
+int	ft_toupper(int c)
+{
+	int	a_lowercase;
 
-#endif
+	a_lowercase = 'a';
+	if (c >= 'a' && c <= 'z')
+	{
+		return (c - a_lowercase + 'A');
+	}
+	else
+	{
+		return (c);
+	}
+}
