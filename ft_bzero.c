a625c7cc4455d064c636f18f7a89a3ad6d7ed7b8
Agustin Lanusse <lanusseagustin@gmail.com>
2023-09-14 18:15:46 +0200

    ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,26 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/13 10:44:20 by aglanuss          #+#    #+#             */
-/*   Updated: 2023/09/14 15:48:17 by aglanuss         ###   ########.fr       */
+/*   Created: 2023/09/14 15:48:32 by aglanuss          #+#    #+#             */
+/*   Updated: 2023/09/14 18:09:45 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-int			ft_isalpha(int c);
-int			ft_isdigit(int c);
-int			ft_isalnum(int c);
-int			ft_isascii(int c);
-int			ft_isprint(int c);
-size_t		ft_strlen(const char *s);
-void		*ft_memset(void *b, int c, size_t len);
-void		ft_bzero(void *s, size_t n);
+void	ft_bzero(void *s, size_t n)
+{
+	char		*pos;
+	size_t		i;
 
-#endif
+	pos = s;
+	i = 0;
+	while (i < n)
+	{
+		pos[i] = '\0';
+		i++;
+	}
+}
+
+int	main()
+{
+	char	str[] = "this is a char array";
+
+	printf("string before > %s\n", str);
+
+	ft_bzero(&str[6], 4 * sizeof(char));
+
+	printf("string after > %s\n", str);
+	return 0;
+}
