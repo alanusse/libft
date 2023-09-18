7c28981c55f7b39d16ab6430cac51e938e109d9d
Agustín Lanusse <lanusseagustin@gmail.com>
2023-09-18 18:14:06 +0200

    ft_memmove

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ LIB = ar -rcs
 
 SRC = ft_bzero.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c ft_memcpy.c ft_memset.c \
 		ft_strlen.c ft_tolower.c ft_toupper.c ft_strchr.c ft_strrchr.c ft_strncmp.c ft_strnstr.c ft_atoi.c ft_atoi.c \
-		ft_strlcat.c
+		ft_strlcat.c ft_memmove.c
 OBJ = $(SRC:.c=.o)
 
 INCLUDE = libft.h
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: agustinlanusse <agustinlanusse@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 10:44:20 by aglanuss          #+#    #+#             */
-/*   Updated: 2023/09/17 17:23:21 by agustinlanu      ###   ########.fr       */
+/*   Updated: 2023/09/18 14:55:04 by agustinlanu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
 size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
 void		*ft_memset(void *b, int c, size_t len);
 void		*ft_memcpy(void *dst, const void *src, size_t n);
+void    *ft_memmove(void *dst, const void *src, size_t len);
 void		ft_bzero(void *s, size_t n);
 
 #endif
