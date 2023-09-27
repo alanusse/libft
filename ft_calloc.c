db6c2ee8476cad2c766e9fa0e8c103e7e7aeaa78
Agustin Lanusse <lanusseagustin@gmail.com>
2023-09-27 13:54:29 +0200

    ft_calloc

---
+++ ft_calloc.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/27 13:01:51 by aglanuss          #+#    #+#             */
+/*   Updated: 2023/09/27 13:52:07 by aglanuss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+#include <stdlib.h>
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, count * size);
+	return (ptr);
+}
+
+// int	main()
+// {
+// 	char *ptr;
+// 	ptr = ft_calloc(5, sizeof(char));
+
+// 	ptr[0] = 'H';
+// 	ptr[1] = 'o';
+// 	ptr[2] = 'l';
+// 	ptr[3] = 'a';
+
+// 	printf("%s", ptr);
+// 	return 1;
+// }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 10:44:20 by aglanuss          #+#    #+#             */
-/*   Updated: 2023/09/20 12:57:26 by aglanuss         ###   ########.fr       */
+/*   Updated: 2023/09/27 13:01:34 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,6 @@ void		*ft_memmove(void *dst, const void *src, size_t len);
 void		*ft_memchr(const void *s, int c, size_t n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 void		ft_bzero(void *s, size_t n);
+void		*ft_calloc(size_t count, size_t size);
 
 #endif
