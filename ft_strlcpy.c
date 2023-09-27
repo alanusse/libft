96ed04acd9fe0c024f6375122d12d00f11a4d531
Agustin Lanusse <lanusseagustin@gmail.com>
2023-09-27 13:00:03 +0200

    ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,41 +6,49 @@
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 12:55:16 by aglanuss          #+#    #+#             */
-/*   Updated: 2023/09/20 14:50:39 by aglanuss         ###   ########.fr       */
+/*   Updated: 2023/09/27 12:57:53 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <string.h>
 #include <stdlib.h>
 #include "libft.h"
 
-size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-  size_t i;
+	size_t	i;
 
-  i = 0;
-  while(i < dstsize)
-  {
-    dst[i] = src[i];
-    i++;
-  }
-  dst[i] = '\0';
-  return ft_strlen(src) + i;
+	if (dstsize == 0)
+		return (ft_strlen(src));
+	i = 0;
+	while (i < (dstsize - 1) && src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(src));
 }
 
-int main()
-{
-  char  dst[] = "Hola mundo!";
-  char  src[] = "Esto es el nuevo string.";
+// int main()
+// {
+// 	char dst[] = "hola";
+// 	char ft_dst[] = "hola";
+// 	char *src = "lorem ipsum dolor sit amet";
+// 	int	size = 5;
 
-  printf("Before strlcpy:\n");
-  printf("dst -> %s\n", dst);
-  printf("src -> %s\n", src);
-  printf("strlcpy -> %lu\n\n", strlcpy(dst, src, sizeof(dst)));
+// 	printf("Before strlcpy:\n");
+// 	printf("dst 	-> %s\n", dst);
+// 	printf("ft_dst  -> %s\n", dst);
+// 	printf("src     -> %s\n\n", src);
 
-  printf("After strlcpy:\n");
-  printf("dst -> %s\n", dst);
-  printf("src -> %s\n", src);
-  return 1;
-}
+// 	printf("strlcpy    -> %lu\n", strlcpy(dst, src, size));
+// 	printf("ft_strlcpy -> %lu\n\n", ft_strlcpy(ft_dst, src, size));
+
+// 	printf("After strlcpy:\n");
+// 	printf("dst    -> %s\n", dst);
+// 	printf("ft_dst -> %s\n", ft_dst);
+// 	printf("src    -> %s\n", src);
+// 	return 1;
+// }
