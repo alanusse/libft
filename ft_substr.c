d5774562dceb542e96d302ddc780b0954fc99f07
Agustín Lanusse <lanusseagustin@gmail.com>
2023-09-28 16:17:53 +0200

    ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,40 +6,51 @@
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 16:08:18 by aglanuss          #+#    #+#             */
-/*   Updated: 2023/09/27 17:04:20 by aglanuss         ###   ########.fr       */
+/*   Updated: 2023/09/28 16:17:34 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// #include <stdio.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*ptr;
+	char		*ptr;
 	size_t	i;
-	int		strlen;
 
-	strlen = ft_strlen(s);
-	if (len == 0 || s[0] == '\0' || start > strlen)
-		return (ptr);
 	i = 0;
-	ptr = (char *)ft_calloc((len + 1), sizeof(char));
+	if (start >= ft_strlen(s))
+	{
+		ptr = (char *)malloc(sizeof(char));
+		if (!ptr)
+			return (NULL);
+		ptr[0] = '\0';
+		return (ptr);
+	}
+	if (len >= ft_strlen(&s[start]))
+		ptr = (char *)malloc((ft_strlen(&s[start]) + 1) * sizeof(char));
+	else
+		ptr = (char *)malloc((len + 1) * sizeof(char));
 	if (!ptr)
-		return (NULL);
-	while (i < len)
+			return (NULL);
+	while (i < len && s[start + i] != '\0')
 	{
 		ptr[i] = s[start + i];
 		i++;
 	}
+	ptr[i] = '\0';
 	return (ptr);
 }
 
 // int main()
 // {
-// 	char *str = "";
-// 	char *ret = ft_substr(str, 0, 1);
-
-// 	printf("ret: %s\n", ret);
+// 	printf("test 1: %s\n", ft_substr("hola como estas", 5, 4)); // output: como
+// 	printf("test 2: %s\n", ft_substr("hola como estas", 0, 15)); // output: hola como estas
+// 	printf("test 3: %s\n", ft_substr("hola como estas", 0, 20000)); // output: hola como estas
+// 	printf("test 4: %s\n", ft_substr("hola", 4, 0)); // output: ""
+// 	printf("test 5: %s\n", ft_substr("hola", 5, 10)); // output: ""
+// 	printf("test 6: %s\n", ft_substr("hola", 10, 10)); // output: ""
+// 	printf("test 7: %s\n", ft_substr("hola", 3, 20)); // output: a
 // 	return 1;
 // }
