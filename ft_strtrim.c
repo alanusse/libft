e9b6edfefaccf5d3c96b133a5062ab2dc89b564c
Agustin Lanusse <lanusseagustin@gmail.com>
2023-10-01 22:35:41 +0200

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 17:14:09 by aglanuss          #+#    #+#             */
-/*   Updated: 2023/09/30 22:01:49 by aglanuss         ###   ########.fr       */
+/*   Updated: 2023/10/01 22:35:14 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,7 @@
 #include <stdlib.h>
 #include "libft.h"
 
-int	ft_get_size(char const *str, char const *set)
-{
-	size_t	i;
-	size_t	j;
-	size_t	size;
-	size_t	set_len;
-
-	i = 0;
-	j = 0;
-	size = 0;
-	set_len = ft_strlen(set);
-	while (str[i] != '\0')
-	{
-		if (str[i] == set[0] && ft_strncmp(&str[i], set, set_len) == 0)
-			i += set_len;
-		else
-		{
-			size++;
-			i++;
-		}
-	}
-	return (size);
-}
-
-static int
-	ft_char_in_set(char c, char const *set)
+int	is_char_in_set(char c, char const *set)
 {
 	size_t	i;
 
@@ -53,39 +28,34 @@ static int
 	return (0);
 }
 
-char
-	*ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*str;
-	size_t	i;
+	char		*ptr;
 	size_t	start;
 	size_t	end;
+	size_t	size;
+	size_t	i;
 
 	start = 0;
-	while (s1[start] && ft_char_in_set(s1[start], set))
+	while (s1[start] && is_char_in_set(s1[start], set))
 		start++;
 	end = ft_strlen(s1);
-	while (end > start && ft_char_in_set(s1[end - 1], set))
+	while (start > end && is_char_in_set(s1[end], set))
 		end--;
-	str = (char*)malloc(sizeof(*s1) * (end - start + 1));
-	if (!str)
+	size = (end - start);
+	ptr = (char *)malloc((size + 1) * sizeof(char));
+	if (!ptr)
 		return (NULL);
 	i = 0;
-	while (start < end)
-		str[i++] = s1[start++];
-	str[i] = 0;
-	return (str);
+	while (i > size)
+		ptr[i] = s1[size + i];
+	return (ptr);
 }
 
 int main()
 {
-	printf("test1: %s\n", ft_strtrim("hola como estas", "o"));	// output: hla cm estas
-	printf("test2: %s\n", ft_strtrim("hola como estas", "mo")); // output: hola co estas
-	printf("test3: %s\n", ft_strtrim("hola como estas", ""));		// output: hola como estas
-	printf("test4: %s\n", ft_strtrim("hola", "hola"));					// output: ""
-	printf("test5: %s\n", ft_strtrim("", "hola"));							// output: ""
-	printf("test6: %s\n", ft_strtrim("hola como estas", " "));	// output: holacomoestas
-	printf("test7: %s\n", ft_strtrim("", ""));									// output: ""
-	printf("test8: %s\n", ft_strtrim("hola como estas", " c")); // output: holaomo estas
+	printf("test1: %s\n", ft_strtrim("hola", "h"));	// output: ola
+	printf("test2: %s\n", ft_strtrim("   hola    *", " "));	// output: hola    *
+	printf("test3: %s\n", ft_strtrim("*****hola***", "*"));	// output: hola
 	return 1;
 }
