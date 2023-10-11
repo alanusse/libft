222da2e48e1f366ee384b612d3b4aa18c76ad000
Agustin Lanusse <lanusseagustin@gmail.com>
2023-10-11 20:15:00 +0200

    split

--- ft_split.c
+++ ft_split.c
@@ -6,39 +6,53 @@
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 22:54:53 by aglanuss          #+#    #+#             */
-/*   Updated: 2023/10/06 18:48:22 by aglanuss         ###   ########.fr       */
+/*   Updated: 2023/10/11 20:14:47 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft.h"
 
-static int	count_strings(char const *str, char c)
+static int	count_words(char const *str, char c)
 {
-	int		count;
-	int		i;
-	int		last_count;
+	int	count;
+	int	ch;
 
-	if (ft_strlen(str) == 0 || ft_strlen(str) == 1)
-		return (1);
 	count = 0;
-	i = 0;
-	while (str[i])
+	ch = 0;
+	while (*str)
 	{
-		if (str[i] == c)
+		if (*str != c && ch == 0)
 		{
-			last_count = i;
 			count++;
+			ch = 1;
 		}
-		i++;
+		else if (*str == c)
+			ch = 0;
+		str++;
 	}
-	if (last_count < ft_strlen(str))
-		count++;
 	return (count);
 }
 
+
+
 char	**ft_split(char const *s, char c)
 {
+	char	*ptr;
+	int		i;
+
+	if (s[0] == '\0')
+		return (ft_strdup(""));
+
+	ptr = (char*)malloc((count_words(s, c) + 1) * sizeof(char*));
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (ptr[i])
+	{
+
+		i++;
+	}
 }
 
 void	ft_putstr(unsigned int i, char *str)
@@ -46,49 +60,29 @@ void	ft_putstr(unsigned int i, char *str)
 	printf("%s", str);
 }
 
-int main()
+int main(void)
 {
-	printf("test1 -> %i\n", count_strings("hola que tal", ' ')); // output: 3
-	printf("test2 -> %i\n", count_strings("", ' '));						 // output: 1
-	printf("test3 -> %i\n", count_strings(" ", ' '));						 // output: 1
-	printf("test4 -> %i\n", count_strings("abcd", ' '));				 // output: 1
-	printf("test5 -> %i\n", count_strings("abcd", 'a'));				 // output: 2
-	printf("test6 -> %i\n", count_strings("a", 'a'));				 // output: 2
-
-
-
 	// output: hola
-	// ft_striteri(ft_split("hola", " "), ft_putstr);
+	ft_striteri(ft_split("hola", ' '), ft_putstr);
 
-	// // output: ""
-	// ft_striteri(ft_split("", ""), ft_putstr);
-
-	// // output: ""
-	// ft_striteri(ft_split("", " "), ft_putstr);
+	// output: ""
+	ft_striteri(ft_split("", ' '), ft_putstr);
 
 	// output:
 	// hola
 	// que
 	// tal
-	// ft_striteri(ft_split("hola que tal", " "), ft_putstr);
+	ft_striteri(ft_split("hola que tal", ' '), ft_putstr);
 
-	// // output:
-	// // bc
-	// // bc
-	// // bc
-	// ft_striteri(ft_split("abcabcabc", "a"), ft_putstr);
+	// output:
+	// bc
+	// bc
+	// bc
+	ft_striteri(ft_split("abcabcabc", 'a'), ft_putstr);
 
-	// // output:
-	// // ""
-	// // abc
-	// // ""
-	// ft_striteri(ft_split("  abc  ", " "), ft_putstr);
+	// output:
+	// abc
+	ft_striteri(ft_split("  abc  ", ' '), ft_putstr);
 
-	// // output:
-	// // a
-	// // b
-	// // c
-	// // d
-	// ft_striteri(ft_split("abcd", ""), ft_putstr);
 	return 1;
 }
