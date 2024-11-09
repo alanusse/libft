af12fe4a111638b0c181285f85adf52534cf3299
Agustín Lanusse <alanusse@agustins-macbook-pro.home>
2024-11-09 23:03:22 +0100

    added ft_bzero function

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,27 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agulanus <agulanus@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/09 17:44:46 by agulanus          #+#    #+#             */
-/*   Updated: 2024/11/09 22:51:54 by agulanus         ###   ########.fr       */
+/*   Created: 2024/11/09 22:51:25 by agulanus          #+#    #+#             */
+/*   Updated: 2024/11/09 22:56:44 by agulanus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdlib.h>
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-
-#endif
+	if (n == 0)
+		return ;
+	i = -1;
+	while (++i < n)
+		((unsigned char *)s)[i] = '\0';
+}
