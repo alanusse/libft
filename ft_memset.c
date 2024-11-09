7ab8a844315c12d342ec1ce49790d82bbfc8baff
Agustín Lanusse <alanusse@agustins-macbook-pro.home>
2024-11-09 22:49:08 +0100

    fix ft_memset typo on function name

--- ft_memset.c
+++ ft_memset.c
@@ -6,13 +6,13 @@
 /*   By: agulanus <agulanus@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 19:05:03 by agulanus          #+#    #+#             */
-/*   Updated: 2024/11/09 22:45:03 by agulanus         ###   ########.fr       */
+/*   Updated: 2024/11/09 22:48:11 by agulanus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memset(void *b, int c, size_t len)
+void	*ft_memset(void *b, int c, size_t len)
 {
 	size_t	i;
 
