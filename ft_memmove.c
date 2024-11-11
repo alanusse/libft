8d26041744bd6480da5b75abb1ad1304407f05dc
alanusse <lanusseagustin@gmail.com>
2024-11-11 16:05:57 +0100

    modified ft_memmove function: optimized function

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: agulanus <agulanus@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 01:53:19 by agulanus          #+#    #+#             */
-/*   Updated: 2024/11/11 15:42:07 by agulanus         ###   ########.fr       */
+/*   Updated: 2024/11/11 16:05:31 by agulanus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 /**
  * Copy byte string.
- * 
+ *
  * The ft_memmove() function copies len bytes from string src to string dst.
  * The two strings may overlap; the copy is always done in a non-destructive
  * manner.
- * 
+ *
  * The ft_memmove() function returns the original value of dst.
  */
 void	*ft_memmove(void *dst, const void *src, size_t len)
@@ -28,16 +28,11 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 
 	if (!dst && !src)
 		return (NULL);
-	if (len == 0)
-		return (dst);
 	d = (unsigned char *)dst;
 	s = (unsigned char *)src;
 	if (d > s)
-	{
-		while (--len > 0)
+		while (len--)
 			d[len] = s[len];
-		d[len] = s[len];
-	}
 	else
 		dst = ft_memcpy(dst, src, len);
 	return (dst);
