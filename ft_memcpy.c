4a7cec7fb623e9ff0cfa981d0ccfeca91f9d281f
alanusse <lanusseagustin@gmail.com>
2024-11-11 16:02:45 +0100

    modified ft_memcpy function: optimized function

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: agulanus <agulanus@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 00:27:35 by agulanus          #+#    #+#             */
-/*   Updated: 2024/11/10 01:52:01 by agulanus         ###   ########.fr       */
+/*   Updated: 2024/11/11 16:01:11 by agulanus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,22 @@
 
 /**
  * Copy memory area.
- * 
+ *
  * The ft_memcpy() function copies n bytes from memory area src to
  * memory area dst. If dst and src overlap, behavior is undefined.
- * 
+ *
  * The ft_memcpy() function returns the original value of dst.
  */
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t	i;
+	unsigned char	*s;
+	unsigned char	*d;
 
 	if (!dst && !src)
 		return (NULL);
-	i = -1;
-	while (++i < n)
-		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+	s = (unsigned char *)src;
+	d = (unsigned char *)dst;
+	while (n--)
+		*d++ = *s++;
 	return (dst);
 }
