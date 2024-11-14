c10f7d27242798ead42e7a9c5bf42b4948aeb97c
alanusse <lanusseagustin@gmail.com>
2024-11-14 16:12:46 +0100

    added ft_calloc function

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: agulanus <agulanus@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 17:44:46 by agulanus          #+#    #+#             */
-/*   Updated: 2024/11/14 01:23:11 by agulanus         ###   ########.fr       */
+/*   Updated: 2024/11/14 01:32:19 by agulanus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,6 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
