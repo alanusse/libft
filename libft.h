5242428002d9ebcf3bd1cc03fa4ae8ad8848abc9
alanusse <lanusseagustin@gmail.com>
2024-11-14 16:28:00 +0100

    added ft_strdup function

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: agulanus <agulanus@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 17:44:46 by agulanus          #+#    #+#             */
-/*   Updated: 2024/11/14 01:32:19 by agulanus         ###   ########.fr       */
+/*   Updated: 2024/11/14 16:18:07 by agulanus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,6 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
 
 #endif
