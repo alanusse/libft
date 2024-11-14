40adaf56581f1c7842139fda8299c4db5e0e5c2f
alanusse <lanusseagustin@gmail.com>
2024-11-14 16:27:35 +0100

    added ft_strdup function

---
+++ ft_strdup.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agulanus <agulanus@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 16:15:23 by agulanus          #+#    #+#             */
+/*   Updated: 2024/11/14 16:26:33 by agulanus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/**
+ * Save a copy of a string.
+ *
+ * The ft_strdup() function allocates sufficient memory for a copy of the
+ * string s1, does the copy, and returns a pointer to it. The pointer may
+ * subsequently be used as an argument to the function free(3).
+ * 
+ * If insufficient memory is available, NULL is returned and errno is set
+ * to ENOMEM.
+ */
+char	*ft_strdup(const char *s1)
+{
+	char	*str;
+	size_t	len;
+
+	len = ft_strlen(s1);
+	str = (char *)ft_calloc(len + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	return (ft_memcpy(str, s1, len));
+}
