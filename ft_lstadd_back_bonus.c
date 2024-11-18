f9be3f13a138c7e111b8b616d41120cbb516239a
alanusse <lanusseagustin@gmail.com>
2024-11-18 10:26:44 +0100

    added comment to ft_lstadd_back function

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,12 +6,15 @@
 /*   By: agulanus <agulanus@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 10:19:13 by agulanus          #+#    #+#             */
-/*   Updated: 2024/11/18 10:24:21 by agulanus         ###   ########.fr       */
+/*   Updated: 2024/11/18 10:26:26 by agulanus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * Adds the node ’new’ at the end of the list.
+ */
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*current;
